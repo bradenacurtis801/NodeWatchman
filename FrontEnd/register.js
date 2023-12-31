c4d93dc73f7c55ff6bf20221ff38408ed1d57ca2
DataCare <datacare@a250101.tail8544d.ts.net>
2023-12-31 11:17:39 -0700

    updates

--- FrontEnd/register.js
+++ FrontEnd/register.js
@@ -4,7 +4,7 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
     const data = Object.fromEntries(formData.entries());
 
     try {
-        const response = await fetch('http://192.168.200.54:3000/register', {
+        const response = await fetch(`http://10.10.250.101:3000/register`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
