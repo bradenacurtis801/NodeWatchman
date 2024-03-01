b406ed0a14c75a9cdf922828db6c066040c2f864
DataCare <datacare@a250101.tail8544d.ts.net>
2024-03-01 11:17:27 -0700

    initial testing for /SSH-Server/server passed. Can use gui from /FrontEnd/server.js and select machines to run bash shell scripts

--- SSH-Server/test.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-exec('ls', (e, stdout, stderr) => {
-    if (e instanceof Error) {
-        console.error(e);
-        throw e;
-    } 
-    console.log('stdout ',stdout);
-    console.log('stderr ',stderr);
-})
