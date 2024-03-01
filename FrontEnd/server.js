80757f92012c0530bf80b2b4164c133691998d40
braden curtis <braden@Ubuntu22.04.myguest.virtualbox.org>
2024-02-29 20:32:34 -0700

    changes

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -10,10 +10,11 @@ app.use(cors({
 
 // Serve static files from the 'FrontEnd' directory
 app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static('public'))
 
 // Route to serve the home.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/home.html'));
+    res.sendFile(path.join(__dirname, 'public/test.html'));
 });
 
 const PORT = 5500;
