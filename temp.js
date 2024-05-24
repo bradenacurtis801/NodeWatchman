643e01e5b7ea6d8633f00bed65835a10cae4d016
bradenacurtis801 <97850331+bradenacurtis801@users.noreply.github.com>
2024-05-24 11:10:51 -0600

    Delete temp.js

--- temp.js
+++
@@ -1,9 +0,0 @@
-// Define a decorator function
-function myDecorator(target) {
-    target.myProperty = 'Hello World!';
-  }
-  // Define a class and apply the decorator
-  @myDecorator
-  class MyClass {}
-  // Access the decorated property
-  console.log(MyClass.myProperty); // Output: Hello World!
